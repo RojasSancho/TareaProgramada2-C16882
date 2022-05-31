a9bd42ef946d51da7e7a18d7e2b0592db53f02ea
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-05-30 19:34:36 -0600

    comentarios corregidos

--- src/planilla.h
+++ src/planilla.h
@@ -15,12 +15,12 @@ class Planilla {
     map<int, Empleado *> indiceEmpleados;
 
     public:
-    Planilla(istream *streamPersonas); //Informacion de la raiz, es decir, el director
+    Planilla(istream *streamPersonas); //Agregar informacion basica de empleados (datos que no tienen que ver con pago)
 
     
 
     void AgregarPagoMensualBruto(istream *streamNomina); //Agregar informacion de empleados de nomina(nomina.txt)
-    void AgregarMontoPorHoraYHorasTrabajadas(istream *streamHorasTrabajadas); //Agragar informacion de empleados por hora(horastrabajadas.txt)
+    void AgregarMontoPorHoraYHorasTrabajadas(istream *streamHorasTrabajadas); //Agregar informacion de empleados por hora(horastrabajadas.txt)
     void CrearReporte();
 
     ~Planilla();
